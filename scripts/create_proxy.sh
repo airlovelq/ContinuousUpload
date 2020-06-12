731af43e89c4761c657a5efeb8838ccf6303bf00
airlovelq <luqin212@163.com>
2020-06-12 11:11:37 +0800

    fix bug in generate config file

--- scripts/create_proxy.sh
+++ scripts/create_proxy.sh
@@ -1,7 +1,7 @@
 source conf/.env
 
 cp conf/storage_template.yaml conf/storage.yaml
-sed -ri "s/HOST_DATA_DIR/$HOST_DATA_DIR/g" conf/storage.yaml
+sed -ri "s!HOST_DATA_DIR!$HOST_DATA_DIR!g" conf/storage.yaml
 sed -ri "s/HOST_DATA_SERVER/$HOST_DATA_SERVER/g" conf/storage.yaml
 sed -ri "s/STORAGE_SIZE/$STORAGE_SIZE/g" conf/storage.yaml
 
@@ -13,7 +13,7 @@ sed -ri "s/DOCKER_PORT/$DOCKER_PORT/g" conf/service.yaml
 
 cp conf/deployment_template.yaml conf/deployment.yaml
 sed -ri "s/VERSION/$VERSION/g" conf/deployment_template.yaml
-sed -ri "s/DOCKER_DATA_DIR/$DOCKER_DATA_DIR/g" conf/deployment_template.yaml
+sed -ri "s!DOCKER_DATA_DIR!$DOCKER_DATA_DIR!g" conf/deployment_template.yaml
 sed -ri "s/DOCKER_PORT/$DOCKER_PORT/g" conf/deployment_template.yaml
 
 kubectl apply -f conf/storage.yaml
