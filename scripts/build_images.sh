dc0372d55dfbe9ce6db4963b0c2cf0995eb6fd74
airlovelq <luqin212@163.com>
2020-06-12 11:18:13 +0800

    fix bug in generate config file

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -1,4 +1,4 @@
 source conf/.env
 
-docker build -t continuous-upload-proxy -f ./dockerfiles/worker.dockerfile \
+docker build -t continuous-upload-proxy:$VERSION -f ./dockerfiles/worker.dockerfile \
     --build-arg DOCKER_DATA_DIR=$DOCKER_DATA_DIR $PWD || exit 1
